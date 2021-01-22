0d754890be81a853ca787c42bfbf516f3c971cb4
Denis <deniszelenko95@gmail.com>
2021-01-22 19:17:14 +0200

    textColor add new activity add new text

---
+++ app/src/main/java/com/example/moneytrackerfirst/AddItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.moneytrackerfirst;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+        setTitle(R.string.add_item_title);
+
+    }
+}
--- app/src/main/java/com/example/moneytrackerfirst/MainActivity.java
+++ app/src/main/java/com/example/moneytrackerfirst/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.moneytrackerfirst;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity","onStop");
     }
 }
